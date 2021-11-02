f1600219a3f1a5976c867fcb440931a6f5c9bae2
Jonathan Afong <jonny560a@hotmail.com.com>
2021-11-02 18:41:06 -0400

    implement typewriter

---
+++ typewriter.js
@@ -0,0 +1,13 @@
+const sentence = "hello there from lighthouse labs";
+
+let delay = 1000;
+for (const char of sentence) {
+    setTimeout(() => {
+        // print the char here
+        process.stdout.write(char);
+      }, delay) // <= 1s delay to make it noticeable. Can dial it down later.
+    delay += 50;
+}
+setTimeout(() => {
+    console.log('\n');
+}, delay);
